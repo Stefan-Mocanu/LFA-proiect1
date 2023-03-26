671e7ba6192a390fbb2f6954d744f8513fe59d5a
Stefan-Mocanu <stefanmo.sm@gmail.com>
2023-03-26 16:11:25 +0300

    Implementare corecta NFA + teste

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-with open("input1.txt", "r") as f:
+with open("input4.txt", "r") as f:
     tranzitii = {x: {} for x in f.readline().strip().split()}
     alfabet = [x for x in f.readline().split()]
     initiala = [f.readline().strip()]
@@ -11,27 +11,33 @@ with open("input1.txt", "r") as f:
         for lit in alfabet:
             if lit not in tranzitii[x].keys():
                 tranzitii[x][lit] = []
-#print(tranzitii)
-f = open("teste1.txt", "r")
+print(tranzitii)
+f = open("teste4.txt", "r")
 x = f.readline()[:-1]
 
 while x:
     drumuri = [initiala.copy()]
     for litera in x:
+        if not drumuri:
+            print(f"{x} nu e acceptat")
+            break
         aux = drumuri.copy()
         drumuri = []
         for drum in aux:
-            stari = list(set(tranzitii[drum[-1]][litera]))
+            stari = tranzitii[drum[-1]][litera]
             if stari:
-                for elem in stari:
-                    drumuri.append(drum)
-                    drumuri[-1].append(elem)
-    for elem in drumuri:
-        if elem[-1] in finale:
-            string = " ".join(elem)
-            print(f"Acceptat cu drumul: {string}")
-            break
+                for elem in range(len(stari)):
+                    drumuri.append(drum + [stari[elem]])
+                    #drumuri[-1].append(stari[elem])
     else:
-        print("Nu e acceptat")
+        ok1 = 1
+        for elem in drumuri:
+            if elem[-1] in finale:
+                string = " ".join(elem)
+                print(f"{x} e acceptat cu drumul: {string}")
+                ok1 = 0
+        if ok1:
+            print(f"{x} nu e acceptat")
+    print()
     x = f.readline()[:-1]
 f.close()
